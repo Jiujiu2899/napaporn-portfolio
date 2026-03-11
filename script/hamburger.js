42a8d074253a4f3f46f461353f1032b37b34723c
jiujiu2899 <jiujiu281999@gmail.com>
2026-03-11 22:42:06 +0700

    responsive 1024px to 835px

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,18 +1,14 @@
-// const hamburger = document.getElementById('hamburger');
-// const navShow = document.getElementById('myNav');
-
-// // hamburger.addEventListener('click', () => {
-// //   navShow.classList.toggle('show');
-// // });
-
-// function hamburgerShow() {
-//   hamburger.classList.toggle("show");
-// }
-// navShow.onclick = hamburgerShow;
 
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
+const navMenu = document.getElementById("myNav");
+const navLi = document.querySelectorAll("#myNav .js-navBtn");
 
 hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("show");
+});
+
+navLi.forEach(el => {
+  el.addEventListener("click", () => {
+    navMenu.classList.remove("show");
+  });
 });
